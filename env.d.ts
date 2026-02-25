161165cc96444c29784805df2c21fa9b97088cf1
Dan Garner <dan@xibosignage.com>
2026-02-25 16:09:06 +0000

    Chore: single source of version for packaging.

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_LOCAL_SERVER_URL: string
   readonly VITE_LOCAL_SERVER_PORT: string
+  readonly VITE_ALIAS_LIBRARIES: boolean
   // more env variables...
 }
 
