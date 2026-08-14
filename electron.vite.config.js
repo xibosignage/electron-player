fe56e4722ee0ce2f6005f0a8051929ce11ad690d
Nadz Mangandog <nadzmangandog@gmail.com>
2026-08-14 18:06:37 +0800

    Migrate/Update to Supported Electron version (41+)
    relates to xibosignage/electron-player#163

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -20,8 +20,7 @@
  */
 import {resolve} from 'path';
 import {readFileSync} from 'node:fs';
-import {defineConfig, externalizeDepsPlugin, bytecodePlugin}
-  from 'electron-vite';
+import {defineConfig} from 'electron-vite';
 
 export default defineConfig(({mode}) => {
   const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
@@ -49,9 +48,9 @@ export default defineConfig(({mode}) => {
       server: {
         hmr: false,
       },
-      plugins: [externalizeDepsPlugin({exclude: devLibraries})],
       define: versionDefine,
       build: {
+        externalizeDeps: {exclude: devLibraries},
         sourcemap: true,
         minify: false,
         rollupOptions: {
@@ -73,6 +72,8 @@ export default defineConfig(({mode}) => {
     },
     preload: {
       build: {
+        externalizeDeps: {exclude: devLibraries},
+        bytecode: true,
         rollupOptions: {
           external: [],
         },
@@ -82,7 +83,6 @@ export default defineConfig(({mode}) => {
           ...alias,
         },
       },
-      plugins: [externalizeDepsPlugin({exclude: devLibraries}), bytecodePlugin()],
     },
     renderer: {
       server: {
